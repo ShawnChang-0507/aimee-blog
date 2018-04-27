395c8967630d63fde50725e88885255def8b6ebd
JerryChang <378090670.qq.com>
2018-04-27 17:12:17 +0800

    修改springboot2.0.1为2.0.0，前者引入thymeleaf会报错，未找到原因=。=

--- src/main/java/com/fullexception/AliciaDoApplication.java
+++ src/main/java/com/fullexception/AliciaDoApplication.java
@@ -2,11 +2,18 @@ package com.fullexception;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class AliciaDoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AliciaDoApplication.class, args);
 	}
+	@GetMapping("/hello")
+	public String getHello(){
+		return "index";
+	}
+	
 }
--- src/main/java/com/fullexception/controller/UserController.java
+++ src/main/java/com/fullexception/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.fullexception.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 	
+	@GetMapping(value="/index")
+	public String showIndex(){
+		return "index/hello"; 
+	}
 }
