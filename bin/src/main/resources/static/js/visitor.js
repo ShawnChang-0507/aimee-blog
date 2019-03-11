0cfc7401f1d421dfe2cacb4c82a522b2160e7a0a
ShawnChang-0507 <sanbowla@163.com>
2019-03-11 16:02:09 +0800

    title显示调整

--- bin/src/main/resources/static/js/visitor.js
+++ bin/src/main/resources/static/js/visitor.js
@@ -1,5 +1,23 @@
 $(function(){
-	$("#link-blog").click(function(){
-		
+	$.ajax({
+		url: functionLocation,
+		type: "POST",
+		data: dataSource,
+		async: async,
+		dataType: "JSON",
+		success: function(data){
+			if (data.functionName == null){
+				showMsg(data.mes)
+				if (data.res == 'true'){
+					window.location.href=window.location.href
+					window.location.reload
+				}
+			}
+			else{
+				if (typeof(eval(data.functionName)) == "function"){
+					eval(data.functionName + "('" + JSON.stringify(data) + "')")
+				}
+			}
+		}
 	})
-})
+}
